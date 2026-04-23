65897069beb19b6e9fe6c0bf1dfcfa0277e7bb62
olelouer <olivier.lelouer@protonmail.com>
2026-04-23 13:02:53 +0200

    add shooting and split

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
+
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
+from shot import Shot
 
 
 def main():
@@ -16,8 +21,14 @@ def game():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, drawable, updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
     while True:
         log_state()
         updatable.update(dt)
@@ -25,6 +36,16 @@ def game():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
         for el in drawable:
             el.draw(screen)
         pygame.display.flip()
